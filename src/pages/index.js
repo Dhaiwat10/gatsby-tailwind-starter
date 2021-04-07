00037a033e6d53625bc06ed202a4f93abed9b74f
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2021-04-07 10:19:12 +0530

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+// markup
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Home Page</title>
+      <h1 className='text-blue text-2xl p-16 text-center'>Hello World</h1>
+    </main>
+  )
+}
+
+export default IndexPage
